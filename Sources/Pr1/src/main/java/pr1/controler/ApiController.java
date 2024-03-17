18daa99db8923106827db7e685ccde015398a455
max g <gmaxg02@gmail.com>
2024-03-17 20:11:13 +0100

    wip deploy ph1

--- Sources/Pr1/src/main/java/pr1/controler/ApiController.java
+++ Sources/Pr1/src/main/java/pr1/controler/ApiController.java
@@ -1,6 +1,8 @@
-package pr1.controler;
+package pr1.controller;
 
 import pr1.kafka.KafkaSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,11 +10,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/import")
 public class ApiController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
+
     @Autowired
     private KafkaSender kafkaSender;
 
-    @PostMapping("/import")
+    @PostMapping("/send")
     public void sendJsonToKafka(@RequestBody String json) {
+        logger.info("Receiving JSON to send to Kafka: {}", json);
         kafkaSender.send("topic1", json);
+        logger.info("JSON sent to Kafka topic successfully.");
     }
 }
--- Sources/Pr1/src/main/java/pr1/kafka/KafkaSender.java
+++ Sources/Pr1/src/main/java/pr1/kafka/KafkaSender.java
@@ -1,5 +1,7 @@
 package pr1.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -7,10 +9,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaSender {
 
+    private static final Logger logger = LoggerFactory.getLogger(KafkaSender.class);
+
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    public void send(String topic, String payload) {
-        kafkaTemplate.send(topic, payload);
+    public void send(String topic, String message) {
+        logger.debug("Sending message='{}' to topic='{}'", message, topic);
+        kafkaTemplate.send(topic, message);
+        logger.info("Message sent to topic successfully.");
     }
 }
