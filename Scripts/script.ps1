83dd7cffd598fff1cfa7c7b2d0e9a2fc18aff694
rob1juan <robinjuan59@gmail.com>
2024-03-18 22:53:52 +0100

    [FIX] refont script

--- Scripts/script.ps1
+++ Scripts/script.ps1
@@ -1,36 +1,47 @@
-# Démarrage des services avec Docker Compose
-docker-compose up -d zookeeper broker
+# Démarrage de Zookeeper et Broker avec Docker Compose
+Invoke-Expression "docker-compose up -d zookeeper broker"
 
 # Attend que Kafka soit prêt
 Write-Host "Attente de Kafka pour être prêt..."
 do {
     Start-Sleep -Seconds 1
-} while (-not(Test-Connection localhost -Port 9092 -Quiet))
+} while (-not (Test-Connection localhost -Port 9092 -Quiet))
 
 Write-Host "Kafka est prêt."
 
 # Création des topics
-docker exec broker kafka-topics --bootstrap-server localhost:9092 --create --replication-factor 1 --partitions 1 --topic topic1
-docker exec broker kafka-topics --bootstrap-server localhost:9092 --create --replication-factor 1 --partitions 1 --topic topic2
-docker exec broker kafka-topics --bootstrap-server localhost:9092 --create --replication-factor 1 --partitions 1 --topic topic3
+Invoke-Expression "docker exec broker kafka-topics --bootstrap-server localhost:9092 --create --replication-factor 1 --partitions 1 --topic topic1"
+Invoke-Expression "docker exec broker kafka-topics --bootstrap-server localhost:9092 --create --replication-factor 1 --partitions 1 --topic topic2"
+Invoke-Expression "docker exec broker kafka-topics --bootstrap-server localhost:9092 --create --replication-factor 1 --partitions 1 --topic topic3"
 
 Write-Host "Topics créés."
 
 # Affichage des topics
-docker exec broker kafka-topics --bootstrap-server localhost:9092 --list
+Invoke-Expression "docker exec broker kafka-topics --bootstrap-server localhost:9092 --list"
 
-# Démarrage de Docker Postgres
-docker-compose up -d postgresdb
+# Démarrage docker Postgres
+Invoke-Expression "docker-compose up -d postgresdb"
 
 # Attend que Postgres soit prêt
 Write-Host "Attente de Postgres pour être prêt..."
 do {
     Start-Sleep -Seconds 1
-} while (-not(Test-Connection localhost -Port 5432 -Quiet))
+} while (-not (Test-Connection localhost -Port 5432 -Quiet))
+
+Write-Host "Postgres est prêt."
+
+# Attendre un peu pour s'assurer que la base de données est prête avec un compte à rebours
+Write-Host "Attente de la base de données pour être prête..."
+$temps_restant = 15
+do {
+    Write-Host "Temps restant : $temps_restant" -NoNewline
+    Start-Sleep -Seconds 1
+    $temps_restant--
+} while ($temps_restant -gt 0)
+Write-Host "La base de données est prête maintenant."
 
 # Création des tables
-# Remplacer "postgresdb" par le nom de votre conteneur PostgreSQL si différent
-docker exec postgresdb psql -U postgres -d HOPSIIA -c "
+$cmdSql = @"
 CREATE TABLE Patient (
     id_patient VARCHAR(255) PRIMARY KEY,
     birthday DATE,
@@ -66,25 +77,24 @@ CREATE TABLE Stay (
     start_time TIMESTAMP,
     end_time TIMESTAMP
 );
-"
+"@
+Invoke-Expression "docker exec postgresdb psql -U postgres -d HOPSIIA -c `"$cmdSql`""
 
 # Affichage des tables
-docker exec postgresdb psql -U postgres -d HOPSIIA -c "\dt"
+Invoke-Expression "docker exec postgresdb psql -U postgres -d HOPSIIA -c '\dt'"
 
-Write-Host "Postgres est prêt."
+# Démarrage docker mirthconnect
+Invoke-Expression "docker-compose up -d mirthconnect"
 
-# Démarrage de Docker Mirth Connect
-docker-compose up -d mirthconnect
-
-# Attend que Mirth Connect soit prêt
-Write-Host "Attente de Mirth Connect pour être prêt..."
+# Attend que mirthconnect soit prêt
+Write-Host "Attente de mirthconnect pour être prêt..."
 do {
     Start-Sleep -Seconds 1
-} while (-not(Test-Connection localhost -Port 8080 -Quiet))
+} while (-not (Test-Connection localhost -Port 8080 -Quiet))
 
-Write-Host "Mirth Connect est prêt."
+Write-Host "mirthconnect est prêt."
 
-docker exec -u root mirthconnect chmod -R 777 /var/echantillon_hl7
+Invoke-Expression "docker exec -u root mirthconnect chmod -R 777 /var/echantillon_hl7"
 
 docker-compose up -d pr1
 docker-compose up -d cr1
